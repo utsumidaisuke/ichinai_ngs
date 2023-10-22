29f340d7f21dd215f78560654383098ff1c611eb
utsumidaisuke <utsumidaisuke@hotmail.com>
2023-10-22 19:55:57 +0900

    change

--- Analysis/03_MRSA_typing/prep/prep_ref.sh
+++ Analysis/03_MRSA_typing/prep/prep_ref.sh
@@ -1,4 +1,5 @@
 # MRSAの参照配列を取得
+mkdir ../gbk
 efetch -db nuccore -id NC_007793 -format gbwithparts > ../gbk/NC_007793.gb
 efetch -db nuccore -id NZ_CP007659 -format gbwithparts > ../gbk/NZ_CP007659.gb
 efetch -db nuccore -id NZ_CP009361 -format gbwithparts > ../gbk/NZ_CP009361.gb
